95790c30c5ea562c692d75f3c9ab89b619f7a289
Raymon Roos <raymon.roos@hotmail.com>
2022-08-31 03:08:52 +0200

    Added all the keybinds from old init.vim
    Made all instances of <cmd> <leader> <cr> etc lowercase for consistency.
    
    Made nmap function accessible for other scripts, to turn my
    zettelkasten config into a pure lua plugin in the future.

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -1,6 +1,6 @@
 -- Functional wrapper for mapping custom keybindings
 local function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
+    local options = { noremap = true, silent = true }
     if opts then
         options = vim.tbl_extend("force", options, opts)
     end
@@ -10,11 +10,11 @@ end
 -- local function vmap(lhs, rhs, opts) map('v', lhs, rhs, opts) end
 local function nmap(lhs, rhs, opts) map('n', lhs, rhs, opts) end
 local function imap(lhs, rhs, opts) map('i', lhs, rhs, opts) end
-local function cmap(lhs, rhs, opts) map('i', lhs, rhs, opts) end
+local function cmap(lhs, rhs, opts) map('c', lhs, rhs, opts) end
 
 -- space as (global) leader key
 vim.g.mapleader = " "
-nmap('<Space>', '<Nop>')
+nmap('<space>', '<nop>')
 
 -- navigate wrapped lines more easily
 nmap('j', 'gj')
@@ -25,16 +25,28 @@ imap('kj', '<esc>')
 cmap('kj', '<C-C>')
 
 -- window navigation
-nmap('<C-h>', '<C-w>h')
-nmap('<C-j>', '<C-w>j')
-nmap('<C-k>', '<C-w>k')
-nmap('<C-l>', '<C-w>l')
+nmap('<c-h>', '<c-w>h')
+nmap('<c-j>', '<c-w>j')
+nmap('<c-k>', '<c-w>k')
+nmap('<c-l>', '<c-w>l')
 
 -- using netrw as a filedrawer
-nmap('<leader>ee', ':exe "Lexplore 15" <CR>', { '<silent>' } )
-nmap('<leader>>', ':exe "vertical resize " . (winwidth(0) * 5/4)<CR>')
-nmap('<leader><', ':exe "vertical resize " . (winwidth(0) * 4/5)<CR>')
-nmap('<leader>+', ':exe "resize " . (winheight(0) * 5/4)<CR>')
-nmap('<leader>-', ':exe "resize " . (winheight(0) * 4/5)<CR>')
+nmap('<leader>ee', '<cmd> Lexplore 15 <cr>')
+nmap('<leader>>', ':exe "vertical resize " . (winwidth(0) * 5/4)<cr>')
+nmap('<leader><', ':exe "vertical resize " . (winwidth(0) * 4/5)<cr>')
+nmap('<leader>+', ':exe "resize " . (winheight(0) * 5/4)<cr>')
+nmap('<leader>-', ':exe "resize " . (winheight(0) * 4/5)<cr>')
 
+nmap('<leader>cd', '<cmd> cd %h <cr> <cmd> pwd <cr>')
 
+nmap('<leader>mm', '<cmd> Mason <cr>')
+nmap('<leader>pp', '<cmd> LspInfo <cr>')
+
+nmap('<leader>xx', '<cmd> TroubleToggle <cr>')
+nmap('<leader>xw', '<cmd> TroubleToggle workspace_diagnostics <cr>')
+nmap('<leader>xd', '<cmd> TroubleToggle document_diagnostics <cr>')
+nmap('<leader>xq', '<cmd> TroubleToggle quickfix <cr>')
+nmap('<leader>xl', '<cmd> TroubleToggle loclist <cr>')
+nmap('<leader>xr', '<Cmd> TroubleToggle lsp_references <cr>')
+
+return nmap
