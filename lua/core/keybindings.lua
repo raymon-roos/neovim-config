838b4daa67529ed6c9f310f3e650f6aa1743ec4e
Raymon Roos <raymon.roos@hotmail.com>
2022-08-31 16:36:46 +0200

    Added more complete window switching bind
    ... using alt modifier
    
    Added bind function wrapper for terminal mode
    
    Added more comments

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -7,10 +7,11 @@ local function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- local function vmap(lhs, rhs, opts) map('v', lhs, rhs, opts) end
+local function vmap(lhs, rhs, opts) map('v', lhs, rhs, opts) end
 local function nmap(lhs, rhs, opts) map('n', lhs, rhs, opts) end
 local function imap(lhs, rhs, opts) map('i', lhs, rhs, opts) end
 local function cmap(lhs, rhs, opts) map('c', lhs, rhs, opts) end
+local function tmap(lhs, rhs, opts) map('t', lhs, rhs, opts) end
 
 -- space as (global) leader key
 vim.g.mapleader = " "
@@ -24,11 +25,29 @@ nmap('k', 'gk')
 imap('kj', '<esc>')
 cmap('kj', '<C-C>')
 
--- window navigation
-nmap('<c-h>', '<c-w>h')
-nmap('<c-j>', '<c-w>j')
-nmap('<c-k>', '<c-w>k')
-nmap('<c-l>', '<c-w>l')
+-- window navigation, in terminal/normal/insert/visual modes
+tmap('<A-h>', '<C-\\><C-N><C-w>h')
+tmap('<A-j>', '<C-\\><C-N><C-w>j')
+tmap('<A-k>', '<C-\\><C-N><C-w>k')
+tmap('<A-l>', '<C-\\><C-N><C-w>l')
+
+imap('<A-h>', '<C-\\><C-N><C-w>h')
+imap('<A-j>', '<C-\\><C-N><C-w>j')
+imap('<A-k>', '<C-\\><C-N><C-w>k')
+imap('<A-l>', '<C-\\><C-N><C-w>l')
+
+nmap('<A-h>', '<C-w>h')
+nmap('<A-j>', '<C-w>j')
+nmap('<A-k>', '<C-w>k')
+nmap('<A-l>', '<C-w>l')
+
+vmap('<A-h>', '<C-w>h')
+vmap('<A-j>', '<C-w>j')
+vmap('<A-k>', '<C-w>k')
+vmap('<A-l>', '<C-w>l')
+
+-- use escape in terminal window like we're used to
+tmap('<Esc>', '<C-\\><C-n>')
 
 -- using netrw as a filedrawer
 nmap('<leader>ee', '<cmd> Lexplore 15 <cr>')
@@ -37,11 +56,14 @@ nmap('<leader><', ':exe "vertical resize " . (winwidth(0) * 4/5)<cr>')
 nmap('<leader>+', ':exe "resize " . (winheight(0) * 5/4)<cr>')
 nmap('<leader>-', ':exe "resize " . (winheight(0) * 4/5)<cr>')
 
+-- switch working dir to directory of current file
 nmap('<leader>cd', '<cmd> cd %:h <cr> <cmd> pwd <cr>')
 
+-- easily open windows of relevant plugins
 nmap('<leader>mm', '<cmd> Mason <cr>')
 nmap('<leader>pp', '<cmd> LspInfo <cr>')
 
+-- controll the diagnostics window
 nmap('<leader>xx', '<cmd> TroubleToggle <cr>')
 nmap('<leader>xw', '<cmd> TroubleToggle workspace_diagnostics <cr>')
 nmap('<leader>xd', '<cmd> TroubleToggle document_diagnostics <cr>')
