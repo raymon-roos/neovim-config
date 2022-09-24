a7c7dd3341923af233b684301f365ace3f2eca0e
Raymon Roos <raymon.roos@hotmail.com>
2022-09-24 14:10:37 +0200

    Refactored null_ls.lua
    Added whitespace to packer/init.lua

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,26 +1,30 @@
-local null_ls = require("null-ls")
+local null_ls = require('null-ls')
+
+local eslintrc = '--config=' .. vim.fn.expand('$XDG_DATA_HOME') .. '/npm/lib/.eslintrc.js'
+local composerBin = vim.fn.expand('$XDG_DATA_HOME') .. '/composer/vendor/squizlabs/php_codesniffer/bin/'
+local composerStandard =  '--standard=~/files/bit-academy/phpcs.xml'
 
 null_ls.setup({
   sources = {
-  -- Register sources for code actions
-    null_ls.builtins.code_actions.eslint_d,
+    -- eslint 
+    null_ls.builtins.code_actions.eslint.with({ extra_args = { eslintrc } }),
+    null_ls.builtins.diagnostics.eslint.with({ extra_args = { eslintrc } }),
+    null_ls.builtins.formatting.eslint.with({ extra_args = { eslintrc } }),
+
+    -- phpcs / phpcbf
+    null_ls.builtins.diagnostics.phpcs.with({
+      command = composerBin .. 'phpcs',
+      extra_args = { composerStandard },
+    }),
+    null_ls.builtins.formatting.phpcbf.with({
+      command = composerBin .. 'phpcbf',
+      extra_args = { composerStandard },
+    }),
+
     null_ls.builtins.code_actions.shellcheck,
     null_ls.builtins.code_actions.gitsigns,
 
-  -- Register sources for diagnostics
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.diagnostics.phpcs.with({
-      command = vim.fn.expand('$XDG_CONFIG_HOME') .. "/composer/vendor/squizlabs/php_codesniffer/bin/phpcs",
-      extra_args = { "--standard=~/files/bit-academy/phpcs.xml" },
-    }),
-
-  -- Register sources for formating
-    null_ls.builtins.formatting.phpcbf.with({
-      command = vim.fn.expand('$XDG_CONFIG_HOME') .. "/composer/vendor/squizlabs/php_codesniffer/bin/phpcbf",
-      extra_args = { "--standard=~/files/bit-academy/phpcs.xml" },
-    }),
-
-  -- register sources for completion
+    -- register sources for completion
     -- null_ls.builtins.completion.phpactor,
   },
 })
