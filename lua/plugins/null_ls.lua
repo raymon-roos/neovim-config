2bbafee1f664a9f3ca6262716b5fa67ef854ed20
Raymon Roos <raymon.roos@hotmail.com>
2022-10-04 13:22:07 +0200

    refactor: reusing builtins property in shorthand

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -4,27 +4,28 @@ local eslintrc = '--config=' .. vim.fn.expand('$XDG_DATA_HOME') .. '/npm/lib/.es
 local composerBin = vim.fn.expand('$XDG_DATA_HOME') .. '/composer/vendor/squizlabs/php_codesniffer/bin/'
 local composerStandard = '--standard=~/files/bit-academy/phpcs.xml'
 
+local b = null_ls.builtins
 null_ls.setup({
   sources = {
     -- eslint 
-    null_ls.builtins.code_actions.eslint.with({ extra_args = { eslintrc } }),
-    null_ls.builtins.diagnostics.eslint.with({ extra_args = { eslintrc } }),
-    null_ls.builtins.formatting.eslint.with({ extra_args = { eslintrc } }),
+    b.code_actions.eslint.with({ extra_args = { eslintrc } }),
+    b.diagnostics.eslint.with({ extra_args = { eslintrc } }),
+    b.formatting.eslint.with({ extra_args = { eslintrc } }),
 
     -- phpcs / phpcbf
-    null_ls.builtins.diagnostics.phpcs.with({
+    b.diagnostics.phpcs.with({
       command = composerBin .. 'phpcs',
       extra_args = { composerStandard },
     }),
-    null_ls.builtins.formatting.phpcbf.with({
+    b.formatting.phpcbf.with({
       command = composerBin .. 'phpcbf',
       extra_args = { composerStandard },
     }),
 
-    null_ls.builtins.code_actions.shellcheck,
-    null_ls.builtins.code_actions.gitsigns,
+    b.code_actions.shellcheck,
+    b.code_actions.gitsigns,
 
     -- register sources for completion
-    -- null_ls.builtins.completion.phpactor,
+    -- b.completion.phpactor,
   },
 })
