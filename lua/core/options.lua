274c7eb199083a80928ef37be2322d017a5ecccb
Raymon Roos <raymon.roos@hotmail.com>
2022-10-17 20:44:39 +0200

    fix: temporarily disabled treesitter autofolding

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,8 +4,8 @@ local g = vim.g
 g.filetype = 'plugin indent on'
 g.syntax_on = true
 
-o.foldmethod = 'expr'
-o.foldexpr = 'nvim_treesitter#foldexpr()'
+-- o.foldmethod = 'expr'
+-- o.foldexpr = 'nvim_treesitter#foldexpr()'
 o.foldlevel = 2
 
 o.softtabstop = 4
