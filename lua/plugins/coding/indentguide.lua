93c9be1b8569f5260749d92d01fb38e6923156b7
Raymon Roos <raymon.roos@hotmail.com>
2022-10-28 21:54:41 +0200

    fix(plugin): removed lines from indentblanklines

--- lua/plugins/coding/indentguide.lua
+++ lua/plugins/coding/indentguide.lua
@@ -1,6 +1,5 @@
 require("indent_blankline").setup({
-  show_end_of_line = true,
-  use_treesitter = true, -- Change once tree sitter has been set up
+  use_treesitter = true,
   show_current_context = true,
   show_current_context_start = true,
 })
