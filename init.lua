340eca8ca3e8c44f426d08c2fc348f6dcb7e9cda
Raymon Roos <raymon.roos@hotmail.com>
2022-11-15 13:17:13 +0100

    fix(init): removed unneeded vscode commpat check

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
-if vim.fn.exists('g:vscode') == 1 then
-  require('core.vscode')
-else
-  require('core.options')
-  require('core.autocmds')
-  require('core.keybindings')
-  require('plugins.packer')
-  require('plugins.zettelkasten')
-end
+require('core.options')
+require('core.autocmds')
+require('core.keybindings')
+require('plugins.packer')
+require('plugins.zettelkasten')
