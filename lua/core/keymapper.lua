d07e5de0cbf38554b7323048a8ff6c5eea3c5e74
Raymon Roos <raymon.roos@hotmail.com>
2022-12-25 04:05:18 +0100

    feat(binds): extracted remapper into its own module
    mapper.map() takes a table of modes in which to apply the new keybind,
    to prevent duplication when applying a keybind to multiple modes.
    I.e. normal, visual, insert etc. All helper functions are properly
    type hinted.

---
+++ lua/core/keymapper.lua
@@ -0,0 +1,57 @@
+local keymapper = {}
+
+--- Functional wrapper for mapping custom keybindings
+--- Bind rhs action to lhs mapping for all modes, applying
+--- norecurse, silent and optionally any aditional opts
+--- @param modes table
+--- @param lhs string
+--- @param rhs string
+--- @param opts table?
+--- @return nil
+ function keymapper.map(modes, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+
+  for _, mode in pairs(modes) do
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  end
+end
+
+--- map key in visual mode only
+--- @param lhs string
+--- @param rhs string
+--- @param opts table?
+--- @return nil
+function keymapper.vmap(lhs, rhs, opts) keymapper.map({ 'v' }, lhs, rhs, opts) end
+
+--- map key in normal mode only
+--- @param lhs string
+--- @param rhs string
+--- @param opts table?
+--- @return nil
+function keymapper.nmap(lhs, rhs, opts) keymapper.map({ 'n' }, lhs, rhs, opts) end
+
+--- map key in insert mode only
+--- @param lhs string
+--- @param rhs string
+--- @param opts table?
+--- @return nil
+function keymapper.imap(lhs, rhs, opts) keymapper.map({ 'i' }, lhs, rhs, opts) end
+
+--- map key in command mode only
+--- @param lhs string
+--- @param rhs string
+--- @param opts table?
+--- @return nil
+function keymapper.cmap(lhs, rhs, opts) keymapper.map({ 'c' }, lhs, rhs, opts) end
+
+--- map key in terminal mode only
+--- @param lhs string
+--- @param rhs string
+--- @param opts table?
+--- @return nil
+function keymapper.tmap(lhs, rhs, opts) keymapper.map({ 't' }, lhs, rhs, opts) end
+
+return keymapper
