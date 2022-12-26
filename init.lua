55c0464e4dd4cb99b0f728cc61a31e2cd3b2f373
Raymon Roos <raymon.roos@hotmail.com>
2022-12-26 16:05:27 +0100

    fix(init): changed directory separator from '/' to '.'

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 for _, config in ipairs({
-  'plugins/packer',       -- ./lua/plugins/packer/init.lua
-  'core/options',         -- ./lua/core/options.lua
-  'plugins/zettelkasten', -- ./lua/plugins/zettelkasten.lua
-  'core/autocmds',        -- ./lua/core/autocmds.lua
-  'core/keybindings',     -- ./lua/core/keybindings
+  'plugins.packer',       -- ./lua/plugins/packer/init.lua
+  'core.options',         -- ./lua/core/options.lua
+  'plugins.zettelkasten', -- ./lua/plugins/zettelkasten.lua
+  'core.autocmds',        -- ./lua/core/autocmds.lua
+  'core.keybindings',     -- ./lua/core/keybindings
 }) do
   local ok, err = pcall(require, config)
   if not ok then
