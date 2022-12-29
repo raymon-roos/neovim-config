6383539a5da9ebfe35ed56fc02127ad40865e0e6
Raymon Roos <raymon.roos@hotmail.com>
2022-12-29 22:29:41 +0100

    fix(null_ls): reorganised file layout

--- lua/plugins/lsp/null_ls.lua
+++ lua/plugins/lsp/null_ls.lua
@@ -3,36 +3,36 @@ local null_ls = require('null-ls')
 local phpcs_path = vim.fn.stdpath('data') .. '/mason/bin/'
 local linterRules = '--standard=' .. vim.fn.expand('$HOME') .. '/files/bit-academy/phpcs.xml'
 
-local eslintrc = '--config=' .. vim.fn.expand('$XDG_DATA_HOME') .. '/npm/lib/.eslintrc.js'
+local eslint_extra_args = '--config=' .. vim.fn.expand('$XDG_DATA_HOME') .. '/npm/lib/.eslintrc.js'
+    .. ' --fix-dry-run'
+    .. ' --stdin'
 
 local b = null_ls.builtins
 null_ls.setup({
   sources = {
-    -- js
-    b.code_actions.eslint.with({ extra_args = { eslintrc } }),
-    b.formatting.eslint.with({ extra_args = { eslintrc } }),
-
-    -- php
-    b.diagnostics.phpcs.with({
+    b.diagnostics.eslint_d.with({ extra_args = { eslint_extra_args, } }), -- js
+    b.diagnostics.phpcs.with({ -- php
       command = phpcs_path .. 'phpcs',
-      extra_args = { linterRules },
-    }),
-    b.formatting.phpcbf.with({
+      extra_args = { linterRules }, }
+    ),
+
+    b.code_actions.eslint_d.with({ extra_args = { eslint_extra_args } }), -- js
+    b.code_actions.shellcheck, -- sh
+    b.code_actions.gitsigns, -- git integration
+
+    b.formatting.rustfmt, -- rust
+    b.diagnostics.eslint_d.with({ extra_args = { eslint_extra_args } }), -- js
+    b.formatting.prettierd, -- js(frameworks)/html/css/md
+    b.formatting.sqlfluff.with({ extra_args = { "--dialect", "mysql" } }), -- sql
+    b.formatting.phpcbf.with({ -- php
       command = phpcs_path .. 'phpcbf',
-      extra_args = { linterRules },
-    }),
+      extra_args = { linterRules }, }
+    ),
 
-    -- sql
-    b.formatting.sqlfluff.with({ extra_args = { "--dialect", "mysql" } }),
-
-    b.code_actions.shellcheck,
-    b.code_actions.gitsigns,
-
-    -- rust
-    b.formatting.rustfmt,
-
-    -- register sources for completion
+    -- completion
     -- b.completion.luasnip
   },
+
   border = 'rounded',
+  root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git"),
 })
