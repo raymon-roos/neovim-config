354fee1008d47b7b2e777c1bc216baf970b377ec
Raymon Roos <raymon.roos@hotmail.com>
2023-01-04 23:40:28 +0100

    fix(null_ls): broken args to eslint_d
    Additional flags were being concatenated to the `--config=` flag, rather
    than being separate flags. Pass a table to `extra_args` instead.

--- lua/plugins/lsp/null_ls.lua
+++ lua/plugins/lsp/null_ls.lua
@@ -3,27 +3,29 @@ local null_ls = require('null-ls')
 local phpcs_path = vim.fn.stdpath('data') .. '/mason/bin/'
 local linterRules = '--standard=' .. vim.fn.expand('$HOME') .. '/files/bit-academy/phpcs.xml'
 
-local eslint_extra_args = '--config=' .. vim.fn.expand('$XDG_DATA_HOME') .. '/npm/lib/.eslintrc.js'
-    .. ' --fix-dry-run'
-    .. ' --stdin'
+local eslint_extra_args = {
+  '--config=' .. vim.fn.expand('$XDG_DATA_HOME') .. '/npm/lib/.eslintrc.js',
+  ' --fix-dry-run',
+  ' --stdin',
+}
 
 local b = null_ls.builtins
 null_ls.setup({
   sources = {
-    b.diagnostics.eslint_d.with({ extra_args = { eslint_extra_args, } }), -- js
+    b.diagnostics.eslint_d.with({ extra_args = eslint_extra_args, }), -- js
     b.diagnostics.phpcs.with({ -- php
       command = phpcs_path .. 'phpcs',
       extra_args = { linterRules }, }
     ),
 
-    b.code_actions.eslint_d.with({ extra_args = { eslint_extra_args } }), -- js
+    b.code_actions.eslint_d.with({ extra_args = eslint_extra_args }), -- js
     b.code_actions.shellcheck, -- sh
     b.code_actions.gitsigns, -- git integration
 
     b.formatting.rustfmt, -- rust
-    b.diagnostics.eslint_d.with({ extra_args = { eslint_extra_args } }), -- js
     b.formatting.prettierd, -- js(frameworks)/html/css/md
     b.formatting.sqlfluff.with({ extra_args = { "--dialect", "mysql" } }), -- sql
+    b.formatting.eslint_d.with({ extra_args = eslint_extra_args }), -- js
     b.formatting.phpcbf.with({ -- php
       command = phpcs_path .. 'phpcbf',
       extra_args = { linterRules }, }
