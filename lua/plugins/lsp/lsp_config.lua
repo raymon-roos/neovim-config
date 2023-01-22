998aaff40615f62b8f9b1c5448df95f652da730d
Raymon Roos <raymon.roos@hotmail.com>
2023-01-22 20:50:31 +0100

    refactor(lsp): moved on_attach callback into separate file

--- lua/plugins/lsp/lsp_config.lua
+++ lua/plugins/lsp/lsp_config.lua
@@ -18,30 +18,7 @@ util.default_config = vim.tbl_deep_extend('force',
   }
 )
 
-local on_attach = function(_, bufnr)
-  local function map(lhs, rhs)
-    vim.keymap.set('n', lhs, rhs, { buffer = bufnr, remap = false, silent = true, })
-  end
-
-  map("gD", vim.lsp.buf.declaration)
-  map("gd", vim.lsp.buf.definition)
-  map("<leader>D", vim.lsp.buf.type_definition)
-  map("K", vim.lsp.buf.hover)
-  map("gI", vim.lsp.buf.implementation)
-  map("<C-k>", vim.lsp.buf.signature_help)
-  map("gR", vim.lsp.buf.references)
-  map("<leader>wa", vim.lsp.buf.add_workspace_folder)
-  map("<leader>wr", vim.lsp.buf.remove_workspace_folder)
-  map("<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end)
-  map("<leader>rn", vim.lsp.buf.rename)
-  map("<leader>ca", vim.lsp.buf.code_action)
-  map("gl", vim.diagnostic.open_float)
-  map("[d", vim.diagnostic.goto_prev)
-  map("]d", vim.diagnostic.goto_next)
-  map("<leader>f", function() vim.lsp.buf.format { async = true } end)
-
-  vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
-end
+local on_attach = require('plugins.lsp.on_attach_callback')
 
 local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup_handlers({
