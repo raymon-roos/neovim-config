684b1ef4cf0256302e3091a7bf69b43c58474f7d
Raymon Roos <raymon.roos@hotmail.com>
2023-02-26 23:05:54 +0100

    fix(ftplugin): textwidth in mails to 72 characters

--- after/ftplugin/mail.lua
+++ after/ftplugin/mail.lua
@@ -1,7 +1,7 @@
 local bo = vim.bo
 local wo = vim.wo
 
-bo.textwidth = 78
-bo.fo = vim.bo.fo .. "wa"  -- Automaticly hardbreak sentences at 72 characters, as long as there are trailing spaces. Period without space denotes new paragraph.
+bo.textwidth = 72          -- Automaticly hardbreak sentences at 72 characters, as long as
+bo.fo = vim.bo.fo .. "wa"  -- there are trailing spaces. Period without space denotes new paragraph.
 wo.spell = true
 bo.spelllang = 'en_gb,nl'
