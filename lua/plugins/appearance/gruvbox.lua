c223eaea4e8769e56e81a8d0ddad77541d311bb6
Raymon Roos <raymon.roos@hotmail.com>
2023-03-03 18:18:30 +0100

    feat(colorscheme): no inverse colours on search, italic strings
    Also changed double quotes to single quotes.

--- lua/plugins/appearance/gruvbox.lua
+++ lua/plugins/appearance/gruvbox.lua
@@ -1,4 +1,4 @@
-require("gruvbox").setup({
+require('gruvbox').setup({
   undercurl = true,
   underline = true,
   underlineline = true,
@@ -11,10 +11,10 @@ require("gruvbox").setup({
   invert_signs = false,
   invert_tabline = false,
   invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = 'hard', -- can be "hard", "soft" or empty string
+  inverse = false, -- invert background for search, diffs, statuslines and errors
+  contrast = 'hard', -- can be 'hard', 'soft' or empty string
   overrides = {
-    String = { italic = false },
+    String = { italic = true },
     SignColumn = { bg = 'none' },
     FoldColumn = { bg = 'none' },
     GruvboxGreenSign = { bg = 'none' },
