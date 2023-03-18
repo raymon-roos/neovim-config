642260d12f486d3e0a14e0ef462e084bd86a168e
Raymon Roos <raymon.roos@hotmail.com>
2023-03-19 00:54:42 +0100

    feat(ftplugins): added surround function for markdown links

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,34 @@ local bo = vim.bo
 
 bo.textwidth = 90
 bo.fo = 'tcrqn1'
+
+local get_input = require("nvim-surround.input").get_input
+require("nvim-surround").buffer_setup({
+  surrounds = {
+    ["l"] = {
+      add = function()
+        local link = get_input("Enter link: "):gsub("\n", "")
+        if link then
+          return {
+            { "[" },
+            { "](" .. link .. ")" },
+          }
+        end
+      end,
+      find = "%b[]%b()",
+      delete = "^(%[)().-(%]%b())()$",
+      change = {
+        target = "^()()%b[]%((.-)()%)$",
+        replacement = function()
+          local link = get_input("Enter link: "):gsub("\n", "")
+          if link then
+            return {
+              { "" },
+              { link },
+            }
+          end
+        end,
+      },
+    },
+  },
+})
