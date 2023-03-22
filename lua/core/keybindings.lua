436c266edcea0e5f8afee1b42d36adc49f97c8a5
Raymon Roos <raymon.roos@hotmail.com>
2023-03-22 19:52:55 +0100

    feat(binds): add bind for inserting blank lines

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -21,6 +21,10 @@ nmap('<S-A-k>', '<CMD> move .-2 <CR>')
 vmap('<S-A-j>', ": '<,'>move '>+1 <CR> gvgv")
 vmap('<S-A-k>', ": '<,'>move '<-2 <CR> gvgv")
 
+-- insert empty line
+nmap('<leader>o', function() vim.fn.append(vim.fn.line('.'), '') end)
+nmap('<leader>O', function() vim.fn.append(vim.fn.line('.') - 1, '') end)
+
 -- rapidly abort
 imap('kj', '<esc>')
 cmap('kj', '<C-c>')
