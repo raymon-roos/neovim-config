9995d2e2aa3a8ac383e6e6c80bf495cdf0e6c8c3
Raymon Roos <raymon.roos@hotmail.com>
2023-04-10 15:06:45 +0200

    refactor(ftplugin): change safety check for markdown link surround

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,31 +3,27 @@ local bo = vim.bo
 bo.textwidth = 90
 bo.fo = 'tcrqn1'
 
-local get_input = require("nvim-surround.input").get_input
-require("nvim-surround").buffer_setup({
+local get_input = require('nvim-surround.input').get_input
+require('nvim-surround').buffer_setup({
   surrounds = {
-    ["l"] = {
+    ['l'] = {
       add = function()
-        local link = get_input("Enter link: "):gsub("\n", "")
-        if link then
+        local link = get_input('Enter link: '):gsub('\n', '') or ''
           return {
-            { "[" },
-            { "](" .. link .. ")" },
+            { '[' },
+            { '](' .. link .. ')' },
           }
-        end
       end,
-      find = "%b[]%b()",
-      delete = "^(%[)().-(%]%b())()$",
+      find = '%b[]%b()',
+      delete = '^(%[)().-(%]%b())()$',
       change = {
-        target = "^()()%b[]%((.-)()%)$",
+        target = '^()()%b[]%((.-)()%)$',
         replacement = function()
-          local link = get_input("Enter link: "):gsub("\n", "")
-          if link then
+          local link = get_input('Enter link: '):gsub('\n', '') or ''
             return {
-              { "" },
+              { '' },
               { link },
             }
-          end
         end,
       },
     },
