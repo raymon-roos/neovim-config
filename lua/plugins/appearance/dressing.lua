735f43f8c7ec7c036a4d19ae2cc820ad592f49c5
Raymon Roos <raymon.roos@hotmail.com>
2023-04-23 17:12:25 +0200

    style(dressing): run formatter on dressing config

--- lua/plugins/appearance/dressing.lua
+++ lua/plugins/appearance/dressing.lua
@@ -2,25 +2,19 @@ require('dressing').setup({
   input = {
     -- Set to false to disable the vim.ui.input implementation
     enabled = true,
-
     -- Default prompt string
     default_prompt = "Input:",
-
     -- Can be 'left', 'right', or 'center'
     prompt_align = "left",
-
     -- When true, <Esc> will close the modal
     insert_only = true,
-
     -- When true, input will start in insert mode.
     start_in_insert = true,
-
     -- These are passed to nvim_open_win
     anchor = "SW",
     border = "rounded",
     -- 'editor' and 'win' will default to being centered
     relative = "cursor",
-
     -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
     prefer_width = 30,
     width = nil,
@@ -28,7 +22,6 @@ require('dressing').setup({
     -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
     max_width = { 140, 0.9 },
     min_width = { 20, 0.15 },
-
     buf_options = {},
     win_options = {
       -- Window transparency (0-100)
@@ -36,7 +29,6 @@ require('dressing').setup({
       -- Disable line wrapping
       wrap = false,
     },
-
     -- Set to `false` to disable
     mappings = {
       n = {
@@ -55,30 +47,24 @@ require('dressing').setup({
         ["<Down>"] = "HistoryNext",
       },
     },
-
     override = function(conf)
       -- This is the config that will be passed to nvim_open_win.
       -- Change values here to customize the layout
       return conf
     end,
-
     -- see :help dressing_get_config
     get_config = nil,
   },
   select = {
     -- Set to false to disable the vim.ui.select implementation
     enabled = true,
-
     -- Priority list of preferred vim.select implementations
     backend = { "telescope", "builtin", "fzf_lua", "fzf", "nui" },
-
     -- Trim trailing `:` from prompt
     trim_prompt = true,
-
     -- Options for telescope selector
     -- These are passed into the telescope picker directly. Can be used like:
     -- telescope = require('telescope.themes').get_ivy({...})
-
     telescope = require('telescope.themes').get_cursor({
       initial_mode = 'normal',
       layout_config = {
@@ -92,13 +78,11 @@ require('dressing').setup({
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "cursor",
-
       buf_options = {},
       win_options = {
         -- Window transparency (0-100)
         winblend = 40,
       },
-
       -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
       -- the min_ and max_ options can be a list of mixed types.
       -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
@@ -108,7 +92,6 @@ require('dressing').setup({
       height = nil,
       max_height = { 50, 0.9 },
       min_height = { 8, 0.05 },
-
       -- Set to `false` to disable
       mappings = {
         ["<Esc>"] = "Close",
@@ -117,17 +100,14 @@ require('dressing').setup({
         ["<CR>"] = "Confirm",
         ["<C-j>"] = "Confirm",
       },
-
       override = function(conf)
         -- This is the config that will be passed to nvim_open_win.
         -- Change values here to customize the layout
         return conf
       end,
     },
-
     -- Used to override format_item. See :help dressing-format
     format_item_override = {},
-
     -- see :help dressing_get_config
     get_config = nil,
   },
