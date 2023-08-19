c9b8849c30a803bc00d47eddf506db57ec9d7b42
Raymon Roos <raymon.roos@hotmail.com>
2023-08-19 14:06:14 +0200

    fix(dressing): remove deprecated setting

--- lua/plugins/appearance/dressing.lua
+++ lua/plugins/appearance/dressing.lua
@@ -11,7 +11,6 @@ require('dressing').setup({
     -- When true, input will start in insert mode.
     start_in_insert = true,
     -- These are passed to nvim_open_win
-    anchor = "SW",
     border = "rounded",
     -- 'editor' and 'win' will default to being centered
     relative = "cursor",
@@ -74,7 +73,6 @@ require('dressing').setup({
     -- Options for built-in selector
     builtin = {
       -- These are passed to nvim_open_win
-      anchor = "NW",
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "cursor",
