b51ba6954d7d635290535bc29c9cd658ae23a062
Raymon Roos <raymon.roos@hotmail.com>
2023-08-19 14:06:43 +0200

    style(dressing): single instead of double quotes

--- lua/plugins/appearance/dressing.lua
+++ lua/plugins/appearance/dressing.lua
@@ -3,22 +3,22 @@ require('dressing').setup({
     -- Set to false to disable the vim.ui.input implementation
     enabled = true,
     -- Default prompt string
-    default_prompt = "Input:",
+    default_prompt = 'Input:',
     -- Can be 'left', 'right', or 'center'
-    prompt_align = "left",
+    prompt_align = 'left',
     -- When true, <Esc> will close the modal
     insert_only = true,
     -- When true, input will start in insert mode.
     start_in_insert = true,
     -- These are passed to nvim_open_win
-    border = "rounded",
+    border = 'rounded',
     -- 'editor' and 'win' will default to being centered
-    relative = "cursor",
+    relative = 'cursor',
     -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
     prefer_width = 30,
     width = nil,
     -- min_width and max_width can be a list of mixed types.
-    -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+    -- min_width = {20, 0.2} means 'the greater of 20 columns or 20% of total'
     max_width = { 140, 0.9 },
     min_width = { 20, 0.15 },
     buf_options = {},
@@ -31,19 +31,19 @@ require('dressing').setup({
     -- Set to `false` to disable
     mappings = {
       n = {
-        ["<Esc>"] = "Close",
-        ["q"] = "Close",
-        ["<CR>"] = "Confirm",
-        ["<C-j>"] = "Confirm",
+        ['<Esc>'] = 'Close',
+        ['q'] = 'Close',
+        ['<CR>'] = 'Confirm',
+        ['<C-j>'] = 'Confirm',
       },
       i = {
-        ["<C-c>"] = "Close",
-        ["<CR>"] = "Confirm",
-        ["<C-j>"] = "Confirm",
-        ["<C-p>"] = "HistoryPrev",
-        ["<Up>"] = "HistoryPrev",
-        ["<C-n>"] = "HistoryNext",
-        ["<Down>"] = "HistoryNext",
+        ['<C-c>'] = 'Close',
+        ['<CR>'] = 'Confirm',
+        ['<C-j>'] = 'Confirm',
+        ['<C-p>'] = 'HistoryPrev',
+        ['<Up>'] = 'HistoryPrev',
+        ['<C-n>'] = 'HistoryNext',
+        ['<Down>'] = 'HistoryNext',
       },
     },
     override = function(conf)
@@ -58,7 +58,7 @@ require('dressing').setup({
     -- Set to false to disable the vim.ui.select implementation
     enabled = true,
     -- Priority list of preferred vim.select implementations
-    backend = { "telescope", "builtin", "fzf_lua", "fzf", "nui" },
+    backend = { 'telescope', 'builtin', 'fzf_lua', 'fzf', 'nui' },
     -- Trim trailing `:` from prompt
     trim_prompt = true,
     -- Options for telescope selector
@@ -73,9 +73,9 @@ require('dressing').setup({
     -- Options for built-in selector
     builtin = {
       -- These are passed to nvim_open_win
-      border = "rounded",
+      border = 'rounded',
       -- 'editor' and 'win' will default to being centered
-      relative = "cursor",
+      relative = 'cursor',
       buf_options = {},
       win_options = {
         -- Window transparency (0-100)
@@ -83,7 +83,7 @@ require('dressing').setup({
       },
       -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
       -- the min_ and max_ options can be a list of mixed types.
-      -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
+      -- max_width = {140, 0.8} means 'the lesser of 140 columns or 80% of total'
       width = nil,
       max_width = { 50, 0.4 },
       min_width = { 25, 0.1 },
@@ -92,11 +92,11 @@ require('dressing').setup({
       min_height = { 8, 0.05 },
       -- Set to `false` to disable
       mappings = {
-        ["<Esc>"] = "Close",
-        ["<C-c>"] = "Close",
-        ["q"] = "Close",
-        ["<CR>"] = "Confirm",
-        ["<C-j>"] = "Confirm",
+        ['<Esc>'] = 'Close',
+        ['<C-c>'] = 'Close',
+        ['q'] = 'Close',
+        ['<CR>'] = 'Confirm',
+        ['<C-j>'] = 'Confirm',
       },
       override = function(conf)
         -- This is the config that will be passed to nvim_open_win.
