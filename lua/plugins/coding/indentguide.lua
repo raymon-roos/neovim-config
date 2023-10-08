a8852ae26bf06019fc3d59668a4be2f0987c6529
Raymon Roos <raymon.roos@hotmail.com>
2023-10-08 20:32:30 +0200

    feat(ibl): rework indent-blankline-guide config
    It had some breaking changes going into version 3, so I had to change
    some things.

--- lua/plugins/coding/indentguide.lua
+++ lua/plugins/coding/indentguide.lua
@@ -1,5 +1,14 @@
-require("indent_blankline").setup({
-  use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = true,
+require('ibl').setup({
+  indent = {
+    char =  '·',
+    -- tab_char = '▏',
+    tab_char = '┊',
+    -- highlight = 'comment',
+  },
+  scope = {
+    show_start = false,
+    show_end = false,
+    priority = 2,
+    -- highlight = 'CursorLineSign',
+  },
 })
