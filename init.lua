ff607429754ce83766d5add28fca8d6ef382a76b
Raymon Roos <raymon.roos@hotmail.com>
2024-05-20 18:36:01 +0200

    style: remove ancient file path comments from `init.lua`
    I think these date from my pre-Telescope days. I certainly don't use
    them to navigate by any more, so removing them is long overdue.

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 for _, config in ipairs({
-  'core.autocmds',        -- ./lua/core/autocmds.lua
-  'core.options',         -- ./lua/core/options.lua
-  'core.keybindings',     -- ./lua/core/keybindings.lua
-  'lazy_conf',            -- ./lua/lazy_conf.lua
-  'zettelkasten',         -- ./lua/zettelkasten.lua
+  'core.autocmds',
+  'core.options',
+  'core.keybindings',
+  'lazy_conf',
+  'zettelkasten',
 }) do
   local ok, err = pcall(require, config)
   if not ok then
