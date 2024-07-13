bbf3958e75480fa1d8a960637d02229000cfc9ea
Raymon Roos <raymon.roos@hotmail.com>
2024-07-13 23:07:57 +0200

    feat(plugin): change `leap` labels to be more colemak_dh friendly

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -13,6 +13,7 @@ return {
   },
   config = function(_, _)
     local leap = require('leap')
+    leap.opts.labels = 'oienarst;yulqwfp/.,hxcdvOIENARST:YULQWFP?><HXCDVgmvkbj'
     leap.add_default_mappings(true)
     leap.add_repeat_mappings(';', ',')
   end,
