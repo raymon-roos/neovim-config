38d01901e96c4375b475570553064d5cec8281f6
Raymon Roos <raymon.roos@hotmail.com>
2025-04-03 10:45:19 +0200

    feat(telescope): use vertical layout strategy everywhere
    I found it annoying that long file names or diagnostics and the like in
    the result pane would be cut off in the horizontal layout. The vertical
    layout puts all panes above each other, meaning each pane can use the
    entire floating window's width, which helps with displaying long lines.
    
    I'm also opting to use the same layout for more pickers, mostly the LSP
    related ones. This makes most pickers look consistent. Now only the
    spelling and buffer pickers have a different layout, which makes sense.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,16 +23,13 @@ return {
               ['<C-f>'] = actions.preview_scrolling_down,
             },
           },
-          layout_strategy = 'flex',
+          layout_strategy = 'vertical',
           layout_config = {
-            flex = {
-              horizontal = {
-                preview_width = 0.60,
-              },
-              flip_columns = 180,
-              width = 0.65,
+            vertical = {
+              width = 0.75,
               height = 0.8,
               anchor = 'c',
+              preview_width = 0.75,
             },
           },
         },
@@ -52,7 +49,7 @@ return {
             theme = 'ivy',
             layout_config = {
               height = 24,
-              preview_width = 0.55,
+              preview_width = 0.65,
             },
             mappings = {
               n = {
@@ -68,29 +65,6 @@ return {
             layout_config = {
               height = 30,
               preview_width = 0.50,
-            },
-          },
-          diagnostics = {
-            theme = 'ivy',
-            layout_config = {
-              height = 16,
-              preview_width = 0.45,
-            },
-          },
-          lsp_references = {
-            theme = 'dropdown',
-            layout_config = {
-              width = 0.65,
-              height = 0.35,
-              anchor = 's',
-            },
-          },
-          lsp_workspace_symbols = {
-            theme = 'dropdown',
-            layout_config = {
-              width = 0.65,
-              height = 0.35,
-              anchor = 's',
             },
           },
           spell_suggest = {
