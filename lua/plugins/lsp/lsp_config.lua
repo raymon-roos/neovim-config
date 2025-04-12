37f94ab0ae788b94f752be1e9f69fe91312f2ceb
Raymon Roos <raymon.roos@hotmail.com>
2025-04-12 15:44:08 +0200

    feat: workaround to get rounded borders back around hover windows
    nvim 0.11 introduced a global `border` option, and changed the way
    borders are set for certain floating windows, like hover documentation.
    As a result, hovers have no borders by default which looks pretty
    terrible. But plugins like telescope are designed without a global
    `border` option in mind, and look broken with it enabled. This is a
    minor hack I spotted in a related issue on the telescope github repo
    that wraps the hover function to temporarily enable rounded borders.

--- lua/plugins/lsp/lsp_config.lua
+++ lua/plugins/lsp/lsp_config.lua
@@ -43,12 +43,15 @@ return {
         prefix = '',
       },
     })
-    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-      vim.lsp.handlers.hover, { border = border }
-    )
-    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-      vim.lsp.handlers.signature_help, { border = border }
-    )
+
+    local hover = vim.lsp.buf.hover
+    vim.lsp.buf.hover = function()
+      return hover({
+        border = 'rounded',
+        max_width = math.floor(vim.o.columns * 0.7),
+        max_height = math.floor(vim.o.lines * 0.7),
+      })
+    end
 
     require('lspconfig.ui.windows').default_options.border = border
 
