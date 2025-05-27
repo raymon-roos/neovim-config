7ccf24dc22adbc29df596a2f94cd36359c0389f2
Raymon Roos <raymon.roos@hotmail.com>
2025-05-27 18:41:59 +0200

    fix(ftplugin): set line-ending style for mail filetype to dos (CRLF)

--- after/ftplugin/mail.lua
+++ after/ftplugin/mail.lua
@@ -1,7 +1,8 @@
 local bo = vim.bo
 local wo = vim.wo
 
-bo.textwidth = 72          -- Automaticly hardbreak sentences at 72 characters, as long as
-bo.fo = vim.bo.fo .. "wa"  -- there are trailing spaces. Period without space denotes new paragraph.
+bo.textwidth = 72         -- Automaticly hardbreak sentences at 72 characters, as long as
+bo.fo = vim.bo.fo .. 'wa' -- there are trailing spaces. Period without space denotes new paragraph.
+bo.fileformat = 'dos'     -- \r\n / CRLF is part of the email spec
 wo.spell = true
 bo.spelllang = 'en_gb,nl'
