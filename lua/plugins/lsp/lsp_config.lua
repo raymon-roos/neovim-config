fd7fb54d93dd5af2e5ad5505d198492e5a1e64dc
Raymon Roos <raymon.roos@hotmail.com>
2025-07-03 14:22:47 +0200

    fix(lsp): `diagnostic.config()` instead of deprecated `sign_define()`

--- lua/plugins/lsp/lsp_config.lua
+++ lua/plugins/lsp/lsp_config.lua
@@ -11,24 +11,18 @@ return {
   config = function()
     vim.lsp.set_log_level('off')
 
-    local signs = {
-      'DiagnosticSignError',
-      'DiagnosticSignWarn',
-      'DiagnosticSignHint',
-      'DiagnosticSignInfo',
-    }
-
-    for _, sign in ipairs(signs) do
-      vim.fn.sign_define(sign, { texthl = sign, text = '●', numhl = '' })
-    end
-
     local border = 'rounded'
 
     vim.diagnostic.config({
       -- virtual_text = { prefix = '●' },
       virtual_text = false,
       signs = {
-        active = signs,
+        text = {
+          [vim.diagnostic.severity.ERROR] = '●',
+          [vim.diagnostic.severity.WARN] = '●',
+          [vim.diagnostic.severity.HINT] = '●',
+          [vim.diagnostic.severity.INFO] = '●',
+        },
       },
       update_in_insert = true,
       underline = true,
